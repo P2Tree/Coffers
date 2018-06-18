37b20c6677cd087ed18621793eadd369af0ffa45
PWESiberiaBear <dicksonliuming@gmail.com>
2018-06-19 07:25:39 +0800

    增加删除文章功能； 增加fontawesome图标； 增加登陆功能； 修饰模板逻辑；

--- diary/views.py
+++ diary/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -12,6 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'diary/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -27,6 +29,7 @@ def post_new(request):
 
     return render(request, 'diary/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
@@ -43,3 +46,9 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'diary/post_edit.html', {'form': form})
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
